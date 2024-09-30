b84d6044f2b5da132b706e4af158d0d9acb957cc
Muhammad Ramish <mramish9696@gmail.com>
2024-10-01 03:32:41 +0500

    End Round Sequence Completed

--- Assets/Scripts/SessionManager.cs
+++ Assets/Scripts/SessionManager.cs
@@ -14,10 +14,18 @@ public class SessionManager : MonoBehaviour
     // Boolean to check if the timer is running
     private bool timerRunning = false;
 
+    public GameObject roundStatusObj;
     public GameObject settingsUi;
+    public GameObject roundFinishUi;
     public TMP_InputField timerInputField;
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI roundStatusText;
+    public TextMeshProUGUI nextRoundTimeText;
+    private Animator animator;
 
+
+    private int nextTimerDuration = 5;
+    private int nextTimerElapsed = 0;
 
     // Ensures that only one instance of the SessionManager exists
     private void Awake()
@@ -32,6 +40,15 @@ public class SessionManager : MonoBehaviour
             DontDestroyOnLoad(gameObject); // Persist the instance across scenes
         }
     }
+
+    private void Start()
+    {
+        if (animator == null) {
+
+            animator = roundStatusObj.GetComponent<Animator>();
+        }
+    }
+
     // Coroutine to run the timer every second    
     private IEnumerator RunTimer()
     {
@@ -48,6 +65,12 @@ public class SessionManager : MonoBehaviour
                 // Do something when the time increments, if needed
                 timerText.text = (timerDuration - elapsedTime).ToString();
             }
+            //next round sequence
+            
+            
+            yield return StartCoroutine(EndRoundSequence());
+
+            
 
             // Timer has finished
             Debug.Log("Timer Finished!");
@@ -90,5 +113,60 @@ public class SessionManager : MonoBehaviour
     public void CloseGame() {
 
         Application.Quit();
+    }
+
+    private IEnumerator WaitForAnimationToEnd(string stateName)
+    {
+        roundStatusObj.SetActive(true);
+        Debug.Log("Inside Animation coroutine");
+        // Play the animation
+        animator.Play(stateName);
+
+        // Wait for the animation to start playing
+        yield return new WaitForEndOfFrame();
+
+        // Get the Animator's current state information
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        // Wait until the animation finishes playing
+        while (stateInfo.normalizedTime < 1.0f)
+        {
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            yield return null;
+        }
+
+        // Do something after the animation finishes
+        Debug.Log("Animation Finished!");
+        roundStatusObj.SetActive(false);
+        nextRoundTimeText.enabled = true;
+        
+    }
+
+    private IEnumerator NextRoundTimer() {
+
+        while (nextTimerElapsed < nextTimerDuration)
+        {
+            yield return new WaitForSeconds(1f); // Wait for 1 second
+            nextTimerElapsed++;
+            //Debug.Log("Elapsed Time: " + elapsedTime);
+
+            // Do something when the time increments, if needed
+            nextRoundTimeText.text = "NEXT ROUND IN \n"+(nextTimerDuration - nextTimerElapsed).ToString();
+        }
+        nextTimerElapsed = 0;
+        nextRoundTimeText.enabled = false;
+        StopCoroutine(NextRoundTimer());
+    }
+    private IEnumerator EndRoundSequence() {
+
+        
+        roundStatusText.enabled = true;
+        roundFinishUi.SetActive(true);
+
+        yield return StartCoroutine(WaitForAnimationToEnd("RoundStatus"));
+
+        yield return StartCoroutine(NextRoundTimer());
+
+        roundFinishUi.SetActive(false);
     }
 }
